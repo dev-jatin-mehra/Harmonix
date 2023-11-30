0b659e1555f3e03fb7345756627d7f26386ba0e8
Jatin Mehra <dev.jatin.singh@gmail.com>
2023-11-30 16:52:55 +0530

    heart button changes

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -1,43 +1,56 @@
 //NEW RELEASES FORWARD AND BACKWARD
 
-let TC_left=document.getElementById('TC_left');
-let TC_right=document.getElementById('TC_right');
-let top_song=document.getElementsByClassName('top-song')[0];
+let TC_left = document.getElementById('TC_left');
+let TC_right = document.getElementById('TC_right');
+let top_song = document.getElementsByClassName('top-song')[0];
 
-TC_right.addEventListener('click',()=>{
-    top_song.scrollLeft+=894;
+TC_right.addEventListener('click', () => {
+    top_song.scrollLeft += 894;
 })
 
-TC_left.addEventListener('click',()=>{
-    top_song.scrollLeft-=894;
+TC_left.addEventListener('click', () => {
+    top_song.scrollLeft -= 894;
 })
 
 //ARTIST PANEL
 
-let move_left=document.getElementById('Artist_left');
-let move_right=document.getElementById('Artist_right');
-let main=document.getElementsByClassName('main-artist')[0];
+let move_left = document.getElementById('Artist_left');
+let move_right = document.getElementById('Artist_right');
+let main = document.getElementsByClassName('main-artist')[0];
 
-move_right.addEventListener('click',()=>{
-    main.scrollLeft+=894;
+move_right.addEventListener('click', () => {
+    main.scrollLeft += 894;
 })
 
-move_left.addEventListener('click',()=>{
-    main.scrollLeft-=894;
+move_left.addEventListener('click', () => {
+    main.scrollLeft -= 894;
 })
 
 //ALBUMS PANEL
 
-let album_left=document.getElementById('Album_left');
-let album_right=document.getElementById('Album_right');
-let album=document.getElementsByClassName('main-album')[0];
+let album_left = document.getElementById('Album_left');
+let album_right = document.getElementById('Album_right');
+let album = document.getElementsByClassName('main-album')[0];
 
-album_right.addEventListener('click',()=>{
-    album.scrollLeft+=894;
+album_right.addEventListener('click', () => {
+    album.scrollLeft += 894;
     console.log(`true`);
 })
 
-album_left.addEventListener('click',()=>{
-    album.scrollLeft-=894;
+album_left.addEventListener('click', () => {
+    album.scrollLeft -= 894;
 })
 
+var heart = document.getElementById("heart");
+function Toggle() {
+    if (heart.classList.contains("bi-heart")) {
+        heart.classList.remove("bi-heart");
+        heart.classList.add("bi-heart-fill");
+        heart.style.color = 'red';
+    }
+    else {
+        heart.classList.remove("bi-heart-fill");
+        heart.classList.add("bi-heart");
+        heart.style.color = "";
+    }
+}
